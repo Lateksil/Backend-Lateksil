bf60e19b81d0348f260ba80cce3c5781f3563a2a
Devaaji <devaaji5@gmail.com>
2023-06-08 12:40:55 +0700

    [ADD] Get order Peralatan

--- controllers/peralatanController.js
+++ controllers/peralatanController.js
@@ -2,6 +2,10 @@ import Item from "../models/itemOrder.js";
 import Order from "../models/order.js";
 import Pengujian from "../models/pengujian.js";
 import Peralatan from "../models/peralatan.js";
+import Project from "../models/project.js";
+import Status from "../models/status.js";
+import TeknisiPengujian from "../models/teknisiPengujian.js";
+import Users from "../models/user.js";
 import {
   handleResponseAuthorization,
   handleResponseError,
@@ -104,6 +108,65 @@ export const getAlatPengujianByOrderId = async (req, res) => {
     }
 
     return handleResponseSuccess(res, order);
+  } catch (error) {
+    console.log(error);
+    return handleResponseError(res);
+  }
+};
+
+export const GetOrderPeralatan = async (req, res) => {
+  try {
+    const getAllTeknisiPengujian = await TeknisiPengujian.findAll({
+      include: [
+        {
+          model: Order,
+          include: [
+            {
+              model: Users,
+              attributes: [
+                "id",
+                "full_name",
+                "email",
+                "no_whatsapp",
+                "address",
+                "company_name",
+              ],
+            },
+            {
+              model: Status,
+              as: "status",
+              where: {
+                status_persetujuan: "3",
+                status_transaction: "3",
+              },
+              attributes: [
+                "id",
+                "status_persetujuan",
+                "status_transaction",
+                "status_payment",
+              ],
+            },
+            {
+              model: Project,
+              as: "proyek",
+            },
+            {
+              model: Item,
+              attributes: ["id"],
+              include: [
+                {
+                  model: Pengujian,
+                  attributes: { exclude: ["createdAt", "updatedAt"] },
+                },
+              ],
+              through: { attributes: ["quantity"] },
+            },
+          ],
+        },
+      ],
+    });
+
+    return handleResponseSuccess(res, getAllTeknisiPengujian);
   } catch (error) {
     console.log(error);
     return handleResponseError(res);
