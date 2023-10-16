fc844fe41e8b7f39d7589f272af929ebf6ef86d2
Devaaji <devaaji5@gmail.com>
2023-10-16 10:34:30 +0700

    [ADD] token manager and frontliner

---
+++ middleware/verifyTokenManagerFrontliner.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const { handleResponseAuthorization } = require("../utils/handleResponse.js");
+
+const verifyTokenManagerFrontliner = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return handleResponseAuthorization(
+      res,
+      401,
+      "Token Not Found! Please Check Data in Authorization"
+    );
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return handleResponseAuthorization(
+        res,
+        403,
+        "Token Expired Please login again"
+      );
+    }
+    const role = (req.role = decoded.role);
+    if (role === "manager") {
+      next();
+    } else if (role === "frontliner") {
+      next();
+    } else {
+      return handleResponseAuthorization(
+        res,
+        401,
+        "Token Not Found! Please Check Data in Authorization"
+      );
+    }
+  });
+};
+
+module.exports = verifyTokenManagerFrontliner;
